837d885fadfb1bef7bb0b26c753d4ea9b1dbb7c6
= <liam2robertson@gmail.com>
2021-05-14 23:50:23 +0100

    Set up Next.js

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>wiki-dive</title>
+      </Head>
+      <h1>wiki-dive</h1>
+    </div>
+  )
+}
