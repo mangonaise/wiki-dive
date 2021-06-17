be4cec5c7f97b9440fbf961151510626bdb26427
= <liam2robertson@gmail.com>
2021-06-17 22:16:07 +0100

    Rework about and support pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import styles from './styles/About.module.scss';
+import { motion } from 'framer-motion';
 
 export default function About() {
   return (
@@ -6,26 +9,22 @@ export default function About() {
       <Head>
         <title>About</title>
       </Head>
-      <h2 style={{ margin: 'auto', textAlign: 'center', marginBottom: '24px' }}>
-        Welcome to Wikidive!
-      </h2>
-      <div style={{ margin: 'auto', maxWidth: 'var(--post-width)', lineHeight: '26px' }}>
-        <p>
-          Ahoy there! I'm Liam, and this is my blog.
-          <br/><br/>
-          There are over <strong>6 million articles</strong> on English Wikipedia.
-          My goal is to find the most weird and wonderful ones, and write about them. 
-          <br/><br/>
-          At present, I'm able to write 3 posts a week. They'll show up on Monday, Wednesday and Friday (midnight UTC).
-          Eventually, I hope that this site will be a massive repository of fascinating stories and interesting facts.
-          <br/><br/>
-          If you like this project, there are some simple ways you can <a href="/support">support the blog</a>.
-          <br/><br/>
-          To get in touch or suggest an article, write to <strong>wikidivemail@gmail.com</strong>
-          <br/><br/>
-          Thanks for visiting!
-        </p>
-      </div>
+      <motion.div layoutId="pageInfo" className={styles.header} transition={{ ease: [0.11, 0.91, 0.32, 0.99], duration: 0.25 }}>
+        <h2>Welcome to Wikidive!</h2>
+      </motion.div>
+      <motion.div
+        className={styles.content}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}>
+        <p>Ahoy there! I'm Liam, and this is my blog.</p>
+        <p>There are over <strong>6 million articles</strong> on English Wikipedia.
+          My goal is to find the most weird and wonderful ones, and write about them.</p>
+        <p>At present, I'm able to write 3 posts a week. They'll show up on Monday, Wednesday and Friday (midnight UTC).
+          Eventually, I hope that this site will be a massive repository of fascinating stories and interesting facts.</p>
+        <p>If you like this project, there are some simple ways you can <Link href="/support"><a>support the blog</a></Link>.</p>
+        <p>Thanks for visiting!</p>
+      </motion.div>
     </div>
   )
 }
--- pages/support.tsx
+++ pages/support.tsx
@@ -1,4 +1,8 @@
 import Head from 'next/head';
+import styles from './styles/About.module.scss';
+import { motion } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 export default function Support() {
   return (
@@ -6,32 +10,28 @@ export default function Support() {
       <Head>
         <title>Support</title>
       </Head>
-      <h2 style={{ margin: 'auto', textAlign: 'center', marginBottom: '24px' }}>
-        Support Wikidive
-      </h2>
-      <div style={{ margin: 'auto', maxWidth: 'var(--post-width)', lineHeight: '26px' }}>
-        <p>
-          Wikidive is a personal project that I work on in my spare time. 
-          I want this to be free and with no annoying ads.
-          If you enjoy the blog, you can support it without spending a penny. 
-          But if you have pennies to spare, that's cool too.
-          <br/><br/>
-          The simplest way to help out is to follow and share on{' '}
+      <motion.div layoutId="pageInfo" className={styles.header} transition={{ ease: [0.11, 0.91, 0.32, 0.99], duration: 0.25 }}>
+        <h2>Support Wikidive</h2>
+      </motion.div>
+      <motion.div
+        className={styles.content}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}>
+        <p>If you enjoy the blog, you can support it without spending a penny.
+          But if you have pennies to spare, that's cool too.</p>
+        <p>The simplest way to help out is to follow and share on{' '}
           <a href="https://www.instagram.com/wikidive.blog/" target="_blank">Instagram</a>,{' '}
           <a href="https://twitter.com/wiki_dive" target="_blank">Twitter</a> and{' '}
           <a href="https://www.reddit.com/r/wikidive/" target="_blank">Reddit</a>.{' '}
-          I update these channels when I post, so it's a great way to stay in the loop.
-          <br/><br/>
-          Another free way to support the blog is to suggest an article.
-          It saves me time on curating content. 
-          Send an email to <strong>wikidivemail@gmail.com</strong> with a link to the article. 
-          <br/><br/>
-          If you're feeling extra nice, you can make a{' '}
-          <a href="https://www.paypal.com/paypalme/wikidive" target="_blank">one-time PayPal donation</a>.
-          <br/><br/>
-        </p>
-        <div style={{ textAlign: 'center' }}>❤️</div>
-      </div>
+          It's also a great way to stay updated on new posts.</p>
+        <p>If you're feeling extra nice, you can also <a href="https://ko-fi.com/wikidive">buy me a coffee</a>.</p>
+        <p>Another free way to support the blog is to suggest an article.
+          Send an email to <strong>wikidivemail@gmail.com</strong> with a link!</p>
+        <div style={{ textAlign: 'center' }}>
+          <FontAwesomeIcon icon={faHeart} style={{ width: '18px', color: 'var(--icon-pink)' }} />
+        </div>
+      </motion.div>
     </div>
   )
 }
